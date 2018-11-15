a32da730708f3cf39c6c44b66ff3ef1b5f6af57f
barcochrist <barcochrist@gmail.com>
2018-11-14 22:42:59 -0500

    Initialize mercadolibre API

--- app/src/main/java/com/programacionmaster/mercadolibreapijava/MainActivity.java
+++ app/src/main/java/com/programacionmaster/mercadolibreapijava/MainActivity.java
@@ -3,11 +3,16 @@ package com.programacionmaster.mercadolibreapijava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mercadolibre.android.sdk.Meli;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Meli.setLoggingEnabled(true);
+        Meli.initializeSDK(getApplicationContext());
     }
 }
