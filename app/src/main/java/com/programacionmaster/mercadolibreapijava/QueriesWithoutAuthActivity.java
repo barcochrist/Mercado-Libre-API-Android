1da973d4fc8b746392edb589fd3c0901e040f5d9
barcochrist <barcochrist@gmail.com>
2018-11-17 15:59:16 -0500

    Delete FloatingButton from Queries activities

--- app/src/main/java/com/programacionmaster/mercadolibreapijava/QueriesWithoutAuthActivity.java
+++ app/src/main/java/com/programacionmaster/mercadolibreapijava/QueriesWithoutAuthActivity.java
@@ -1,11 +1,8 @@
 package com.programacionmaster.mercadolibreapijava;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class QueriesWithoutAuthActivity extends AppCompatActivity {
 
@@ -13,16 +10,7 @@ public class QueriesWithoutAuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queries_without_auth);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
